8465f2f7de05ee1e584f46c1b445ee3f5e7d58b8
=Mouhamad Mourtada DIOP <=>
2024-12-30 16:24:55 +0000

    http

--- lib/config.ts
+++ lib/config.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'quincaillerie_back.mcarred.sn:8000/api';
+export const API_URL = 'http://quincaillerie_back.mcarred.sn:8000/api';
 
 export const getAuthHeader = () => {
     const token = localStorage.getItem('token');
